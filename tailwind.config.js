05fae3f33e339830bc67da6e121626020428060e
Kartik Thatikonda <kartikthatikonda@gmail.com>
2025-03-29 21:46:26 +0530

    Fix Tailwind CSS PostCSS plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
-  safelist: [
-    "bg-darkBg",
-    "bg-cardBg",
-    "text-textPrimary",
-    "text-accent"
-  ],
   theme: {
     extend: {},
   },
