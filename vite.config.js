b9a5ccee439cb6dbb51c974869e0dbbac101294a
Kartik Thatikonda <kartikthatikonda@gmail.com>
2025-03-29 21:21:48 +0530

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/", // Change "./" to "/"
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./src/index.css";`,
-      },
-    },
-  },
-})
+});
